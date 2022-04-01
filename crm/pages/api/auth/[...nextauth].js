50c28f49780a7ff19068119010fe395dd4f526e5
Tzuzul Code <mail@tzuzulcode.com>
2022-03-31 21:09:40 -0600

    Inicio de sesion con next auth

--- crm/pages/api/auth/[...nextauth].js
+++ crm/pages/api/auth/[...nextauth].js
@@ -5,6 +5,10 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export default NextAuth({
+    pages:{
+      signIn:"/login",
+      error:"/login"
+    },
     providers:[
         GoogleProvider({
             clientId:process.env.GOOGLE_CLIENT_ID,
@@ -13,11 +17,7 @@ export default NextAuth({
         Credentials({
             async authorize(credentials, req) {
                 await prisma.$connect()
-                console.log(credentials)
 
-                const allUsers = await prisma.user.findMany()
-
-                console.log(allUsers)
                 if(credentials.type==="register"){
                   const userByEmail = await prisma.user.findUnique({
                     where:{
@@ -25,10 +25,7 @@ export default NextAuth({
                     }
                   })
 
-                  console.log(!userByEmail)
-
                   if(!userByEmail){
-                    console.log("true!!!!")
                     const user = await prisma.user.create({
                       data:{
                         email:credentials.email,
@@ -38,32 +35,34 @@ export default NextAuth({
                       }
                     })
 
-                    console.log("User:",user)
-
                     return {
                         email:credentials.email,
                         name:credentials.name,
                         profilePic:credentials.profilePic,
                     }
                   }
+                  
+                }else if(credentials.type==="login"){
+                  const user = await prisma.user.findUnique({
+                    where:{
+                      email:credentials.email
+                    }
+                  })
 
-                  
-                  
+                  if(!user || user.password!==credentials.password){
+                    return null
+                  }
+
+                  return {
+                    email:user.email,
+                    name:user.name,
+                    profilePic:user.profilePic,
+                  }
+
+
                 }
 
                 return null
-                
-      
-          
-                // if (user) {
-                //   // Any object returned will be saved in `user` property of the JWT
-                //   return user
-                // } else {
-                //   // If you return null then an error will be displayed advising the user to check their details.
-                //   return null
-          
-                //   // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-                // }
               }
         })
     ]
--- crm/pages/login.jsx
+++ crm/pages/login.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {useSession,signOut,getCsrfToken} from 'next-auth/react'
 import {AiFillGithub} from "react-icons/ai"
 import {AiFillGoogleCircle} from "react-icons/ai"
 import Input from '../components/input'
+import { useRouter } from 'next/router'
 
 export async function getServerSideProps(context) {
   const csrfToken = await getCsrfToken(context)
@@ -14,21 +15,37 @@ export async function getServerSideProps(context) {
 
 export default function Login({csrfToken}) {
     const {data:session} = useSession()
+
+    const [isLogin,setIsLogin] = useState(true)
+
+    const router = useRouter()
+
+    const {error} = router.query
+
+
   return (
     <>
         <div className='bg-gray-800 w-1/2 mx-auto p-10 mt-10 rounded-md'>
+          <button onClick={()=>{setIsLogin(!isLogin)}}>Cambiar</button>
           <form className='flex flex-col' action='/api/auth/callback/credentials' method='POST'>
               <Input name={"csrfToken"} type={"hidden"} defaultValue={csrfToken}/>
-              <Input name={"type"} type={"hidden"} defaultValue={"login"}/>
+              <Input name={"type"} type={"hidden"} defaultValue={isLogin?"login":"register"}/>
+              {!isLogin&&<Input name={"name"} type={"text"} placeholder={"Nombre..."}/>}
+              {!isLogin&&<Input name={"profilePic"} type={"text"} placeholder={"Profile pic..."}/>}
               <Input name={"email"} type={"email"} placeholder={"Email..."}/>
               <Input name={"password"} type={"pasword"} placeholder={"Password..."}/>
-              <button className='bg-slate-900 text-white p-3 rounded hover:bg-yellow-100 hover:text-black mt-5' type="submit">Iniciar sesión</button>
+              {!isLogin&&<Input name={"confirmPassword"} type={"pasword"} placeholder={"Confirm Password..."}/>}
+              <button className='bg-slate-900 text-white p-3 rounded hover:bg-yellow-100 hover:text-black mt-5' type="submit">{isLogin?"Iniciar sesión":"Registrar"}</button>
           </form>
 
           <form className='flex justify-center text-white' action='/api/auth/signin/google' method='POST'>
             <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
             <button className='bg-slate-900 p-3 rounded hover:bg-yellow-100 hover:text-black' type="submit"><AiFillGoogleCircle className='w-10 h-10 inline-block'/> Inicia sesión con Google</button>
           </form>
+          {error&&<div className='bg-yellow-300'>
+            <p>Revisa tus credenciales</p>
+          </div>
+            }
         </div>
         
     </>
